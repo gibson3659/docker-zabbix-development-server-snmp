2d224bd48836da32a90dfaacea41d67802deb21d
Jason Gibson <jason.gibson@guavus.com>
2014-06-04 23:32:40 -0400

    removed command from run_container.sh

--- run_container.sh
+++ run_container.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -i -t -p 8080:80 -p 10051:10051 -p 162:162 -v /data1/mysqldata:/var/lib/mysql/:rw --name zabbix zabbix2.2 bash
+docker run -i -t -p 8080:80 -p 10051:10051 -p 162:162 -v /data1/mysqldata:/var/lib/mysql/:rw --name zabbix zabbix2.2 
